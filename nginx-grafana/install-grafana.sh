95dfda73d5d31c67b4f14bd445ab90ec2be7e7bd
Olaoluwa Osuntokun <laolu32@gmail.com>
2014-09-15 15:16:30 -0500

    Initial nginx and grafana config

---
+++ nginx-grafana/install-grafana.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GRAFANA_VER=grafana-1.8.0-rc1
+GRAFANA_PKG=$GRAFANA_VER.tar.gz
+GRAFANA_URL=http://grafanarel.s3.amazonaws.com/$GRAFANA_PKG
+
+# Download the latest grafana version
+wget $GRAFANA_URL
+tar xvfz $GRAFANA_PKG
+cp $GRAFANA_VER/* .
+rm $GRAFANA_VER
+
+rm config.sample.js
+
+# Replace placeholder with actual password from container linking.
+sed -i 's/DB_PASSWORD/$INFLUXDB_ENV_INFLUXDB_DEFAULT_DB_PASSWORD/g' config.js
