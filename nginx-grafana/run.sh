1af6f61b5a672e5f148745bb44a411a23903c9a8
Olaoluwa Osuntokun <laolu32@gmail.com>
2014-10-01 14:38:49 -0700

    Target absolute path for pass swap.

--- nginx-grafana/run.sh
+++ nginx-grafana/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace placeholder with actual password from container linking.
-sed -i 's/DB_PASSWORD/${INFLUXDB_ENV_INFLUXDB_DEFAULT_DB_PASSWORD}/g' config.js
+sed -i 's/DB_PASSWORD/${INFLUXDB_ENV_INFLUXDB_DEFAULT_DB_PASSWORD}/g' /root/grafana/config.js
 
 exec /usr/sbin/nginx
 
