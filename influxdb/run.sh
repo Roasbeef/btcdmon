cc256301249987eba4892a52483e2209277f202c
Olaoluwa Osuntokun <laolu32@gmail.com>
2015-08-30 15:03:17 -0700

    Upgrade to InfluxDB version 0.9.2.1

--- influxdb/run.sh
+++ influxdb/run.sh
@@ -1,100 +1,98 @@
 #!/bin/bash
 
 set -m
+CONFIG_FILE="/config/config.toml"
+INFLUX_HOST="localhost"
+INFLUX_API_PORT="8086"
+PRE_CREATE_DB="btcdmon"
+API_URL="http://${INFLUX_HOST}:${INFLUX_API_PORT}"
 
-if [ "${INFLUXDB_ROOT_PASSWORD}" == "**ChangeMe**" ]; then
-    echo "=> No password is specified for InfluxDB root user!"
-    echo "=> Program terminated!"
-    exit 1
+# Dynamically change the value of 'max-open-shards' to what 'ulimit -n' returns
+sed -i "s/^max-open-shards.*/max-open-shards = $(ulimit -n)/" ${CONFIG_FILE}
+
+# Configure InfluxDB Cluster
+if [ -n "${FORCE_HOSTNAME}" ]; then
+    if [ "${FORCE_HOSTNAME}" == "auto" ]; then
+        #set hostname with IPv4 eth0
+        HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
+        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${HOSTIPNAME}\"/g" ${CONFIG_FILE}
+    else
+        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${FORCE_HOSTNAME}\"/g" ${CONFIG_FILE}
+    fi
 fi
 
+if [ -n "${REPLI_FACTOR}" ]; then
+    /usr/bin/perl -p -i -e "s/replication-factor = 1/replication-factor = ${REPLI_FACTOR}/g" ${CONFIG_FILE}
+fi
+
+if [ "${PRE_CREATE_DB}" == "**None**" ]; then
+    unset PRE_CREATE_DB
+fi
+
+# NOTE: It seems this is not used anymore...
+#
+# if [ "${SSL_CERT}" == "**None**" ]; then
+#     unset SSL_CERT
+# fi
+#
+# if [ "${SSL_SUPPORT}" == "**False**" ]; then
+#     unset SSL_SUPPORT
+# fi
+
+# Add UDP support
+if [ -n "${UDP_DB}" ]; then
+    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/false/true/; s/#//g; s/\"udpdb\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
+fi
+if [ -n "${UDP_PORT}" ]; then
+    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
+fi
+
+echo "influxdb configuration: "
+cat ${CONFIG_FILE}
 echo "=> Starting InfluxDB ..."
-exec /usr/bin/influxdb -config=/config/config.toml &
+exec /opt/influxdb/influxd -config=${CONFIG_FILE} &
 
-#wait for the startup of influxdb
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of InfluxDB service startup ..."
-    sleep 3 
-    curl -s -o /dev/null http://localhost:8086/ping
-    RET=$?
-done
-echo ""
-
-
-# check if default root password "root" is still valid
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8086/cluster_admins?u=root&p=root")
-if test $STATUS -eq 200; then
-    # let's update the root password
-    STATUS=$(curl -X POST -s -o /dev/null -w "%{http_code}" "http://localhost:8086/cluster_admins/root?u=root&p=root" -d "{\"password\": \"${INFLUXDB_ROOT_PASSWORD}\"}")
-    if test $STATUS -eq 200; then
-        echo "=> InfluxDB root password successfully updated."
+# Pre create database on the initiation of the container
+if [ -n "${PRE_CREATE_DB}" ]; then
+    echo "=> About to create the following database: ${PRE_CREATE_DB}"
+    if [ -f "/data/.pre_db_created" ]; then
+        echo "=> Database had been created before, skipping ..."
     else
-        echo "=> Failed to update InfluxDB root password!"
-        echo "=> Program terminated!"
-        exit 1
+        arr=$(echo ${PRE_CREATE_DB} | tr ";" "\n")
+
+        #wait for the startup of influxdb
+        RET=1
+        while [[ RET -ne 0 ]]; do
+            echo "=> Waiting for confirmation of InfluxDB service startup ..."
+            sleep 3
+            curl -k ${API_URL}/ping 2> /dev/null
+            RET=$?
+        done
+        echo ""
+
+        PASS=${INFLUXDB_INIT_PWD:-root}
+        if [ -n "${ADMIN_USER}" ]; then
+          echo "=> Creating admin user"
+          /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="CREATE USER ${ADMIN_USER} WITH PASSWORD '${PASS}' WITH ALL PRIVILEGES"
+          for x in $arr
+          do
+              echo "=> Creating database: ${x}"
+              /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="create database ${x}"
+              /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="grant all PRIVILEGES on ${x} to ${ADMIN_USER}"
+          done
+          echo ""
+        else
+          for x in $arr
+          do
+              echo "=> Creating database: ${x}"
+              /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="create database \"${x}\""
+          done
+        fi
+
+        touch "/data/.pre_db_created"
     fi
 else
-    # default root password "root" has already been changed
-    # check if the given one is valid:
-    STATUS=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8086/cluster_admins?u=root&p=${INFLUXDB_ROOT_PASSWORD}")
-    if test $STATUS -eq 200; then
-        echo "=> Password supplied for InfluxDB root user is already ok." 
-    else
-        echo "=> Password supplied for InfluxDB root user is wrong!"
-        echo "=> Program terminated!"
-        exit 1
-    fi
+    echo "=> No database need to be pre-created"
 fi
-
-dbs_to_create=("grafana" "${INFLUXDB_DEFAULT_DB_NAME}")
-for db_to_create in "${dbs_to_create[@]}"; do
-    if [ $dbs_to_create == "**None**" ]; then
-        echo "=> No default database name supplied: no database will be created."
-        continue
-    else
-        curl -s "http://localhost:8086/db?u=root&p=${INFLUXDB_ROOT_PASSWORD}" | grep -q "\"name\":\"$db_to_create\""
-        if [ $? -eq 0 ]; then
-            echo "=> Database \"$db_to_create\" already exists: nothing to do."
-        else
-            echo "=> Creating database: $db_to_create"
-            STATUS=$(curl -X POST -s -o /dev/null -w "%{http_code}" "http://localhost:8086/db?u=root&p=${INFLUXDB_ROOT_PASSWORD}" -d "{\"name\":\"$db_to_create\"}")
-            if test $STATUS -eq 201; then
-                echo "=> Database \"$db_to_create\" successfully created."
-            else
-                echo "=> Failed to create database \"$db_to_create\"!"
-                echo "=> Program terminated!"
-                exit 1
-            fi
-        fi
-
-        if [ "${INFLUXDB_DEFAULT_DB_USER}" == "**None**" ]; then
-            echo "=> No default database user supplied: no user will be created."
-        else
-            curl -s "http://localhost:8086/db/$db_to_create/users?u=root&p=${INFLUXDB_ROOT_PASSWORD}" | grep -q "\"name\":\"$db_to_create\""
-            if [ $? -eq 0 ]; then
-                echo "=> User \"${INFLUXDB_DEFAULT_DB_USER}\" already exists: nothing to do."
-            else
-                if [ "${INFLUXDB_DEFAULT_DB_PASSWORD}" == "**None**" ]; then
-                    echo "=> You must specify a password in INFLUXDB_DEFAULT_DB_PASSWORD env variable!"
-                    echo "=> Program terminated!"
-                    exit 1
-                fi
-                echo "=> Creating user: ${INFLUXDB_DEFAULT_DB_USER}"
-                STATUS=$(curl -X POST -s -o /dev/null -w "%{http_code}" "http://localhost:8086/db/$db_to_create/users?u=root&p=${INFLUXDB_ROOT_PASSWORD}" -d "{\"name\":\"${INFLUXDB_DEFAULT_DB_USER}\", \"password\":\"${INFLUXDB_DEFAULT_DB_PASSWORD}\"}")
-                if test $STATUS -eq 200; then
-                    echo "=> User \"${INFLUXDB_DEFAULT_DB_USER}\" successfully created."
-                    STATUS=$(curl -X POST -s -o /dev/null -w "%{http_code}" "http://localhost:8086/db/$db_to_create/users/${INFLUXDB_DEFAULT_DB_USER}?u=root&p=${INFLUXDB_ROOT_PASSWORD}" -d "{\"admin\":true}")
-                    if test $STATUS -ne 200; then
-                        echo "=> Failed to give admin rights to user: ${INFLUXDB_DEFAULT_DB_USER}"
-                    fi
-                else
-                    echo "=> Failed to create user \"${INFLUXDB_DEFAULT_DB_USER}\"!"
-                    continue
-                fi
-            fi
-        fi
-    fi
-done
 
 fg
