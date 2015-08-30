3399d5881bf3d9c45d37737d1eb31f9e77a56039
Olaoluwa Osuntokun <laolu32@gmail.com>
2015-08-30 15:02:07 -0700

    Scrap old orchestration set up, moving to docker-compose

--- nginx-grafana/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Replace placeholder with actual password from container linking.
-sed -i "s/DB_PASSWORD/$INFLUXDB_ENV_INFLUXDB_DEFAULT_DB_PASSWORD/g" /root/grafana/config.js
-
-exec /usr/sbin/nginx
-
-fg
